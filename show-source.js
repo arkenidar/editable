fdd47ef85d5ff94a4518ddd757f5c7017590c9d5
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2023-01-05 22:02:26 +0100

    little changes

--- show-source.js
+++ show-source.js
@@ -1,4 +1,4 @@
 var text=`<pre contenteditable id=code 
-  style="background-color: yellowgreen; overflow-y: scroll; width: 100%; "></pre>`
+  style="background-color: yellowgreen; overflow-y: scroll; "></pre>`
 document.currentScript.insertAdjacentHTML("afterend", text)
-fetch("").then(res=>res.text()).then(res=>code.innerText=res)
+fetch("").then(res=>res.text()).then(res=>code.textContent=res)
